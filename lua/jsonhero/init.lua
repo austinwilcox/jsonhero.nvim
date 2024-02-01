5301da8a4fe9840c5b7eb3bac4a1bbeb3bb92d7b
Austin Wilcox <awilcox@arbinger.com>
2024-02-01 09:45:21 -0700

    update nvim create command to have an uppercase first letter

--- lua/jsonhero/init.lua
+++ lua/jsonhero/init.lua
@@ -69,7 +69,7 @@ end
 
 --Setup the plugin
 function M.setup()
-  vim.api.nvim_create_user_command("jsonhero", json_hero(), {})
+  vim.api.nvim_create_user_command("Jsonhero", json_hero(), {})
 end
 
 -- open_browser("" .. to_base_64(get_visual_selection()))
